f46bba79f49f4c85469f860da47b52dbb40d924e
shadow <shadow@e3b94c14-b6ff-0310-bb71-eb0c48b0cc48>
2006-06-16 00:33:10 +0000

    remove superfluous stub functions

--- trunk/src/xdaemon/lib/stubs.c
+++ trunk/src/xdaemon/lib/stubs.c
@@ -5,14 +5,6 @@ int _XmaxKeyCode, _XminKeyCode;
 
 EXPENTRY Window Daemon_RootWindow();
 
-char *Daemon_getatomname(Atom atom) {
-	return "";
-}
-Atom Daemon_xinternatom(_Xconst char* atom_name, Bool only_if_exists) {
-	return (Atom)0;
-}
-void Daemon_purgegc(GC gc) {
-}
 int Xlib_State(int hint) {
 	int state = 0;
 
@@ -68,29 +60,4 @@ Bool _XQueryPointer(Window w, Window* root_return, Window* child_return,
 	if(mask_return)
 		*mask_return = Xlib_State(-1);
 	return TRUE;
-}
-void Xlib_FreeAllAtoms(EB_Pixmap *client) {
-}
-void Xlib_UnmonitorResource(XID *resource) {
-}
-void Xlib_set_data(HWND window, Atom dataname, UserData *data) {
-}
-UserData *Xlib_get_data(HWND window, Atom dataname) {
-	return (UserData *)0;
-}
-int XDeleteProperty(Display* dpy, Window window, Atom atom) {
-	return 0;
-}
-EXPENTRY Status XGetWindowAttributes2(Display* display, Window w, XWindowAttributes* window_attributes_return) {
-	return (Status)0;
-}
-EXPENTRY int XGetWindowProperty(Display* display, Window w, Atom property,
-	long long_offset, long long_length, Bool delete, Atom req_type, Atom*
-	actual_type_return,	int* actual_format_return, unsigned long* nitems_return,
-	unsigned long* bytes_after_return, unsigned char** prop_return) {
-	return 0;
-}
-
-EXPENTRY XWindowAttributes *Daemon_getwinattrib(Drawable w, HPS *hps) {
-	return NULL;
 }
